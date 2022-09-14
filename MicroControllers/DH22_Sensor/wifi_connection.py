d5cea76d72cadd5902750d5c8d935c3b118caa80
Jake James-Robinson <95934370+JakeJR0@users.noreply.github.com>
2022-09-14 22:37:22 +0100

    Update wifi_connection.py
    Bug Patch for issue #3
    
    This code ensures the device is connected to the internet which was causing the bug. 
    
    Signed-off-by: Jake James-Robinson <95934370+JakeJR0@users.noreply.github.com>

--- MicroControllers/DH22_Sensor/wifi_connection.py
+++ MicroControllers/DH22_Sensor/wifi_connection.py
@@ -1,14 +1,37 @@
 import network
+from time import sleep
 
-wlan = network.WLAN(network.STA_IF)
+wifi_ssid = ""
+wifi_password = ""
 
-wlan.active(True)
-if wlan.isconnected:
-    print("Device already connected")
-else:
-    wlan.connect("SSID", "Password")
+def connect():
+    """
+        Used to ensure the micro-controller
+        connects to the wifi.
+    """
+    wlan = network.WLAN(network.STA_IF)
 
-if wlan.isconnected:
-    print("Connected to wifi.")
-else:
-    print("Failed to connect to wifi.")
+    wlan.active(True)
+    attempts = 3
+    
+    while True:
+        if attempts <= 0:
+            print("Failed to connect to wifi.")
+            break
+            
+        if wlan.isconnected():
+            print("Connected to wifi.")
+            break
+        else:
+            wlan.connect(wifi_ssid, wifi_password)
+            print("Attempting to connect to the Wifi")
+        
+        sleep(3)
+        
+        if wlan.isconnected():
+            print("Connected to wifi.")
+            break
+        
+        attempts -= 1
+    
+
